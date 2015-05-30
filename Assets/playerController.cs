b4a18056d8cb1c46adbf77ff888e38c85fc6da6e
WereDiggle <archdiggle@gmail.com>
2015-05-29 21:47:37 -0400

    basic player movement, only right

---
+++ Assets/playerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+	
+	public float releaseForce;
+	public float maxReleaseForce;
+	private Rigidbody2D rigidbody2d;
+	public float releaseForceIncreaseRate;
+	public KeyCode left;
+	public KeyCode right;
+
+	enum State{Idle, ChargeRight, ChargeLeft, LaunchRight, LaunchLeft} 
+	private State currentState;
+
+	// Use this for initialization
+	void Start () {
+		currentState = State.Idle;
+		rigidbody2d = GetComponent<Rigidbody2D>(); 
+	
+	}
+	
+	void FixedUpdate () {
+		if (Input.GetKey (right)) {
+			currentState = State.ChargeRight;
+			releaseForce += releaseForceIncreaseRate;
+		} else if (currentState == State.ChargeRight && Input.GetKeyUp (right)) {
+			currentState = State.LaunchRight;
+			float currentRelease = Mathf.Min(releaseForce, maxReleaseForce);
+			rigidbody2d.AddForce (Vector2.right * currentRelease);
+			releaseForce = 0;
+		} 
+		
+	}
+}
