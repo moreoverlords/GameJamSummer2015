c5196e96c6cd9c3f7b9017093d168e72ddc0d807
Eric Hardy <eric.allen.hardy@gmail.com>
2015-05-30 14:33:26 -0400

    really bad obstacle spawning

--- Assets/Generator.cs
+++ Assets/Generator.cs
@@ -10,6 +10,12 @@ public class Generator : MonoBehaviour {
 	public Vector3 bottomOfWorld;
 	public float rowHeight = 50f;
 	public int rowBufferCount = 5;
+	public bool ObstacleNeeded;
+	public GameObject obstacle;
+	public GameObject wall;
+
+	public float leftWallx; //x value for left wall
+	public float rightWallx; //"        " right wall
 
 	// Use this for initialization
 	void Start () {
@@ -30,7 +36,7 @@ public class Generator : MonoBehaviour {
 		//
 		while (topPosition.position.y > (topOfWorld.y - rowBufferCount * rowHeight)) {
 			// TODO: spawn a new row above
-	
+			makeRow (topOfWorld.y, topOfWorld.y + rowHeight);
 			topOfWorld = new Vector3(topOfWorld.x,
 			                                  topOfWorld.y + rowHeight, 
 			                                  topOfWorld.z);
@@ -38,10 +44,36 @@ public class Generator : MonoBehaviour {
 		// check below
 		while (topPosition.position.y < (bottomOfWorld.y + rowBufferCount * rowHeight)) {
 			// TODO: spawn a new row above
-			
+			makeRow (bottomOfWorld.y - rowHeight, bottomOfWorld.y);
 			bottomOfWorld = new Vector3(bottomOfWorld.x,
 			                                     bottomOfWorld.y - rowHeight, 
 			                                     bottomOfWorld.z);
+			}
+	}
+
+	void makeRow(float rowTop, float rowBottom) {
+		if (wall) {
+			int numObstacles = (int) Mathf.Round (Random.Range(0, 5));
+			GameObject leftWall, rightWall;
+			leftWall  = Instantiate (wall, new Vector3 (0,          rowBottom, 0), Quaternion.identity) as GameObject;
+			rightWall = Instantiate (wall, new Vector3 (rightWallx, rowBottom, 0), Quaternion.identity) as GameObject;
+
+			for (int i=0; i<numObstacles; i++) {
+				makeObstacle(rowTop, rowBottom);
+			}
+		}
+	}
+
+	void makeObstacle (float rowTop, float rowBottom) {
+		if (obstacle) {
+			Quaternion rotation = new Quaternion ();
+			rotation.eulerAngles = new Vector3 (0, 0, Random.Range (-180, 180)); 
+
+			Vector3 position = new Vector3 (Random.Range (leftWallx, rightWallx), Random.Range (rowTop, rowBottom), 0);
+			GameObject newObstacle = Instantiate (obstacle, position, rotation) as GameObject;
+
+			Vector3 scale = new Vector3 (Random.Range (0.1f, 4.0f), Random.Range (0.1f, 4.0f), 0f);
+			newObstacle.transform.localScale = scale;
 		}
 	}
 }
