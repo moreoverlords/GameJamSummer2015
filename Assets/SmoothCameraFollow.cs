9143592fe13eadc4fb4cf03141b81f9b9aaa5307
Eric Hardy <eric.allen.hardy@gmail.com>
2015-05-30 09:59:44 -0400

    fixup! camera follows highest player

--- Assets/SmoothCameraFollow.cs
+++ Assets/SmoothCameraFollow.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class SmoothCameraFollow : MonoBehaviour {
 
 	public float dampTime = 0.15f;
+	public float verticalOffset;
 	private Vector3 velocity = Vector3.zero;
 	public Transform player1;
 	public Transform player2;
+
 
 	// Use this for initialization
 	void Start () {
@@ -24,7 +26,9 @@ public class SmoothCameraFollow : MonoBehaviour {
 				target = player2;
 			}
 			Vector3 targetRelPosn = GetComponent<Camera>().WorldToViewportPoint(target.position);
-			Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(targetRelPosn.x, 0.5f, targetRelPosn.z));
+			Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(
+				new Vector3(targetRelPosn.x, verticalOffset, targetRelPosn.z)
+			);
 			Vector3 destination = transform.position + delta;
 			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
 		}
