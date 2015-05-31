7bb4865ba8a9f1b50a94c2fe7cdc15fb95134286
WereDiggle <archdiggle@gmail.com>
2015-05-30 22:55:51 -0400

    crazy eyes

---
+++ Assets/eyeFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eyeFollow : MonoBehaviour {
+
+	public Transform player;
+	public float eyeScale;
+
+	private Vector2 center;
+
+	// Use this for initialization
+	void Start () {
+		center = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 diff = player.position - transform.position;
+		diff = diff / diff.magnitude;
+		transform.localPosition = center + diff * eyeScale;
+
+	
+	}
+}
